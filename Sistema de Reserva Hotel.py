fe2a409e6c94732312150d18decd9504b61eac6c
JonRondriguez <jon.rodriguezl@duocuc.cl>
2026-06-20 02:17:00 -0400

    Add files via upload
    Estructuración de menú

--- Sistema de Reserva Hotel.py
+++ Sistema de Reserva Hotel.py
@@ -23,19 +23,61 @@ def calculo_por_noche(calculo):
 def validar_total(total, categoria):
 
     if total < 200000:
-        categoria = "Economica"
+        return "Economica"
 
     elif total > 200000 and total < 500000:
-        categoria = "Estandar"
+        return "Estandar"
 
     elif total > 500000:
-        categoria = "Premium"
+        return "Premium"
+
+def buscar_reserva(reservas, codigo):    
+    for reserva in reservas:
+        if reserva["codigo"] == codigo:
+            return reserva
+        return None
+#----------------------------MENU PRINCIPAL-------------------------------|
+    print("\t\t-----------------------")
+    print("\t\t|> MENU DE OPCIONES  <|")
+    print("\t\t-----------------------")
 
 
-#------------MENU PRINCIPAL----------|
+    print("\t---------------------------------------------")
+    print("\t|     N°                DESCRIPCION         |")
+    print("\t---------------------------------------------")
+    print(f"\t|    1.-             Registrar reserva     |")
+    print("\t---------------------------------------------")
+    print(f"\t|    2.-             Buscar reserva        |")
+    print("\t---------------------------------------------")
+    print(f"\t|    3.-             Actualizar reserva    |")
+    print("\t---------------------------------------------")
+    print(f"\t|    4.-             Eliminar reserva      |")
+    print("\t---------------------------------------------")
+    print(f"\t|    5.-             Mostrar reservas      |")
+    print("\t---------------------------------------------")
+    print(f"\t|    6.-             Mostrar estadísticas  |")
+    print("\t---------------------------------------------")
+    print(f"\t|    7.-                   Salir           |")
+    print("\t---------------------------------------------")
+    print("")
+
+#------------------BUSCAR RESERVA-----------------------|
+
+def buscar_reserva(reservas, codigo):
+    print("\n===== BUSQUEDA DE RESERVA =====")
+
+    codigo = int(input("Ingrese codigo de reserva: "))
+    if not validacion_codigo:
+        print("Error: Debe ingresar el codigo para validar su reserva")
+        return
 
 
 
+
+
+
+
+ 
 
 
 
